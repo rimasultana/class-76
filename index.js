1a249982e11e2487bed607cf5377c1fe130aef60
rimasultana <rimasultana20225@gmail.com>
2024-03-17 21:25:17 +0600

    call

---
+++ index.js
@@ -0,0 +1,17 @@
+const person = {
+    fullName : function(){
+        return this.firstName + " " + this.lastName;
+    }
+};
+
+const person1 = {
+    firstName: "Rima",
+    lastName: "Sultana"
+};
+
+const person2 = {
+firstName: "Mobasshir",
+lastName: "Khondhoker"
+};
+
+console.log( person.fullName.call(person1 + person2));
